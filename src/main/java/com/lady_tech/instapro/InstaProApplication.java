e18b047cad2bfefb83dae540dfeb6eb5cae66371
zashalovska <zashalovska@gmail.com>
2021-01-04 19:45:38 +0200

    add insta-pro project

---
+++ src/main/java/com/lady_tech/instapro/InstaProApplication.java
@@ -0,0 +1,13 @@
+package com.lady_tech.instapro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lady_tech/instapro/InstaProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lady_tech.instapro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
